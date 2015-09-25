28520d7ad11df4d07646b9c860c374725df2f99f
Devin Stiert <devin.stiert@gmail.com>
2015-09-24 21:38:22 -0400

    Attempt to deal with plugin assembly redirects

--- Bloop.Core/Plugin/CSharpPluginLoader.cs
+++ Bloop.Core/Plugin/CSharpPluginLoader.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Reflection;
 using Bloop.Infrastructure.Logger;
 using Bloop.Plugin;
+using System.IO;
+using System.Xml;
 
 namespace Bloop.Core.Plugin
 {
@@ -18,6 +20,20 @@ namespace Bloop.Core.Plugin
             {
                 try
                 {
+                    // Check if the plugin has an app.config
+                    if(File.Exists(metadata.ExecuteFilePath + ".config"))
+                    {
+                        using (var reader = XmlReader.Create(metadata.ExecuteFilePath + ".config"))
+                        {
+                            // Can't do redirects properly, so we're gonna fake it.
+                            while(reader.ReadToFollowing("assemblyIdentity"))
+                            {
+                                var name = reader.GetAttribute("name");
+                                this.RedirectAssembly(name, metadata.PluginDirectory);
+                            }
+                        }  
+                    }
+
                     Assembly asm = Assembly.Load(AssemblyName.GetAssemblyName(metadata.ExecuteFilePath));
                     List<Type> types = asm.GetTypes().Where(o => o.IsClass && !o.IsAbstract &&  o.GetInterfaces().Contains(typeof(IPlugin))).ToList();
                     if (types.Count == 0)
@@ -49,6 +65,22 @@ namespace Bloop.Core.Plugin
             }
 
             return plugins;
+        }
+
+        private void RedirectAssembly(string shortName, string path)
+        {
+            ResolveEventHandler handler = null;
+
+            handler = (sender, args) => {
+                var requestedAssembly = new AssemblyName(args.Name);
+                if (requestedAssembly.Name != shortName)
+                    return null;
+
+                AppDomain.CurrentDomain.AssemblyResolve -= handler;
+                // Load the assembly of this name from the plugin's directory.
+                return Assembly.LoadFrom(Path.Combine(path, shortName + ".dll"));
+            };
+            AppDomain.CurrentDomain.AssemblyResolve += handler;
         }
     }
 }
