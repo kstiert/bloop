7d2cb3c295df40d15a1f05a4af9b99754e140b56
Devin Stiert <dstiert@cimpress.com>
2015-10-16 16:19:38 -0400

    First stab at python plugin support

---
+++ Bloop.Core/Plugin/IronPythonPluginLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bloop.Plugin;
+using IronPython.Hosting;
+using IronPython.Runtime.Types;
+
+namespace Bloop.Core.Plugin
+{
+    public class IronPythonPluginLoader : IPluginLoader
+    {
+        public IEnumerable<PluginPair> LoadPlugin(List<PluginMetadata> pluginMetadatas)
+        {
+            var plugins = new List<PluginPair>();
+            List<PluginMetadata> PythonPluginMetadatas = pluginMetadatas.Where(o => o.Language.ToUpper() == AllowedLanguage.IronPython.ToUpper()).ToList();
+
+            foreach (var metadata in PythonPluginMetadatas)
+            {
+                var py = Python.CreateRuntime();
+                var scope = py.UseFile(metadata.ExecuteFilePath);
+                var iplugin = scope.GetVariable("IPlugin");
+                dynamic plugin = scope.GetItems().Select(i => (i.Value as PythonType)).FirstOrDefault(i => i != null && i.__subclasscheck__(iplugin));
+                plugins.Add(new PluginPair { Metadata = metadata, Plugin = plugin });
+            }
+            return plugins;
+        }
+    }
+}
