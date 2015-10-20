4343276c04864c7e0e757fba5461beb25ac4fedf
Devin Stiert <devin.stiert@gmail.com>
2015-10-19 20:23:00 -0400

    First pass on registry lookup for windows store apps

---
+++ Plugins/Bloop.Plugin.Program/ProgramSources/WindowsAppSource.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Bloop.Plugin.Program.ProgramSources
+{
+    public class WindowsAppSource : AbstractProgramSource
+    {
+        public override List<Program> LoadPrograms()
+        {
+            var programs = new List<Program>();
+            using(var packages = Registry.ClassesRoot.OpenSubKey(@"HKEY_CLASSES_ROOT\Extensions\ContractId\Windows.Protocol\PackageId"))
+            {
+                foreach(var package in packages.GetSubKeyNames().Select(key => packages.OpenSubKey(key+ "\\ActivatableClassId")).Where(key => key != null))
+                {
+                    foreach(var classId in package.GetSubKeyNames().Select(key => package.OpenSubKey(key)).Where(key => key != null))
+                    {
+                        var properties = classId.OpenSubKey("CustomProperties");
+                        if(properties != null)
+                        {
+                            programs.Add(new Program
+                            {
+                                Title = (string)classId.GetValue("DisplayName"),
+                                ExecutePath = properties.GetValue("Name") + "://"
+                            });
+                        }
+                    }
+                }
+            }
+            return programs;
+        }
+    }
+}
